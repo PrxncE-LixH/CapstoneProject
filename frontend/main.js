dc00646af3543524fb3e89a6bdfe051a94f2ba3c
Robert Quartey <robertquartey@Roberts-MacBook-Air.local>
2023-04-24 23:06:43 -0400

    .

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,29 @@
-
 import axios from "axios";
 
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const form = document.querySelector("form");
 
-axios.post('hhtp://localhost:3000/login', {
-  em
-})
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
+  const userInfo = {
+    email: email.value,
+    password: password.value,
+  };
 
+  try {
+    const user = await axios.post("http://localhost:3000/login", {
+      ...userInfo,
+    });
+    if(user.status === 200){
+      console.log('logded in')
+
+      console.log(user)
+    }
+    
+    console.log(user)
+  } catch (error) {
+    console.log(error);
+  }
+});
