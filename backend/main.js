080c4475848bc9c60151b0c7cd5d1f92b32b09da
Robert Quartey <robertquartey@Roberts-MacBook-Air.local>
2023-05-08 11:34:07 -0400

    .

--- backend/main.js
+++ backend/main.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from 'cors'
 import userRoutes from "./controllers/routes/userRoutes.js";
+import setupJWTStrategy from "./auth/index.js";
+import passport from 'passport'
 const app = express();
 
 app.use(express.json());
 app.use(cors())
+setupJWTStrategy
 app.use("/", userRoutes);
 app.listen(3000, () => {
   console.log("running on port 3000");
--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../../App.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
